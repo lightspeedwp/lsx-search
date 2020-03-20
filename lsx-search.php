173dc3e88345e4e5045d460e782f29359a818dc7
Virginia Garcia <ve.garcia.ortiz@gmail.com>
2020-03-20 18:12:33 -0300

    updating readme

--- lsx-search.php
+++ lsx-search.php
@@ -4,7 +4,7 @@
  * Plugin URI:	https://github.com/lightspeeddevelopment/lsx-search
  * Description:	The LSX Search extension improves the search result pages for LSX Theme.
  * Author:		LightSpeed
- * Version: 	1.3.3
+ * Version: 	1.4
  * Author URI: 	https://www.lsdev.biz/
  * License: 	GPL3
  * Text Domain: lsx-search
@@ -19,9 +19,9 @@ if ( ! defined( 'WPINC' ) ) {
 define( 'LSX_SEARCH_PATH', plugin_dir_path( __FILE__ ) );
 define( 'LSX_SEARCH_CORE', __FILE__ );
 define( 'LSX_SEARCH_URL', plugin_dir_url( __FILE__ ) );
-define( 'LSX_SEARCH_VER', '1.3.3' );
+define( 'LSX_SEARCH_VER', '1.4' );
 
 /* ======================= Below is the Plugin Class init ========================= */
 
-require_once( LSX_SEARCH_PATH . '/classes/class-lsx-search.php' );
-require_once( LSX_SEARCH_PATH . '/includes/functions.php' );
+require_once LSX_SEARCH_PATH . '/classes/class-lsx-search.php';
+require_once LSX_SEARCH_PATH . '/includes/functions.php';
--- readme.txt
+++ readme.txt
@@ -3,9 +3,9 @@ Contributors: feedmymedia
 Donate link: https://donate.lsdev.biz/
 Tags: lsx, search, facetwp, Gutenberg, category
 Requires at least: 5.1.1
-Tested up to: 5.3.2
+Tested up to: 5.4
 Requires PHP: 7.0
-Stable tag: 1.3.3
+Stable tag: 1.4
 License: GPLv3
 License URI: https://www.gnu.org/licenses/gpl-3.0.en.html
 
