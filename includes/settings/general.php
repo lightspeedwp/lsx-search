13072e2fc7b544dd4c8826a4ea5fbf5b175ebd58
Warwick Booth <warwick@lsdev.biz>
2020-07-09 11:37:29 +0200

    Removing the UIX tabs

--- includes/settings/general.php
+++ includes/settings/general.php
@@ -3,11 +3,6 @@
 		<?php if ( class_exists( 'LSX_Currencies' ) ) { ?>
 			<li><a href="#ui-currencies" class="active"><?php esc_html_e( 'Currencies', 'lsx-search' ); ?></a></li>
 		<?php } ?>
-
-		<?php if ( class_exists( 'LSX_Search' ) ) { ?>
-			<?php $class_active = class_exists( 'LSX_Currencies' ) ? '' : 'active' ?>
-			<li><a href="#ui-search" class="<?php echo esc_attr( $class_active ) ?>"><?php esc_html_e( 'Search', 'lsx-search' ); ?></a></li>
-		<?php } ?>
 	</ul>
 
 	<?php if ( class_exists( 'LSX_Currencies' ) ) { ?>
@@ -15,17 +10,6 @@
 			<table class="form-table">
 				<tbody>
 					<?php do_action( 'lsx_framework_dashboard_tab_content', 'currency_switcher' ); ?>
-				</tbody>
-			</table>
-		</div>
-	<?php } ?>
-
-	<?php if ( class_exists( 'LSX_Search' ) ) { ?>
-		<?php $class_active = class_exists( 'LSX_Currencies' ) ? '' : 'active' ?>
-		<div id="ui-search" class="ui-tab <?php echo esc_attr( $class_active ) ?>">
-			<table class="form-table">
-				<tbody>
-					<?php do_action( 'lsx_framework_dashboard_tab_content', 'search' ); ?>
 				</tbody>
 			</table>
 		</div>
