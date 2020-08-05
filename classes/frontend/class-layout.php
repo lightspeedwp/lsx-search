d97500ceb2b3d6c131355aba5e93887854bb7882
Warwick Booth <warwick@lsdev.biz>
2020-08-05 21:46:35 +0200

    Adding in the filters to allow change the layout switcher key and default option.

--- classes/frontend/class-layout.php
+++ classes/frontend/class-layout.php
@@ -50,6 +50,8 @@ class Layout {
 			if ( isset( $lsx_search->frontend->options['display'][ $lsx_search->frontend->search_prefix . '_layout_switcher_enable' ] ) ) {
 				add_filter( 'lsx_blog_customizer_show_switcher', array( $this, 'show_layout_switcher' ), 10, 1 );
 				add_filter( 'lsx_layout_switcher_options', array( $this, 'lsx_layout_switcher_options' ), 10, 1 );
+				add_filter( 'lsx_layout_switcher_page_key', array( $this, 'lsx_layout_switcher_page_key' ), 10, 1 );
+				add_filter( 'lsx_layout_switcher_options_default', array( $this, 'lsx_layout_switcher_options_default' ), 10, 1 );
 			}
 		}
 	}
@@ -72,5 +74,32 @@ class Layout {
 		unset( $layout_options['default'] );
 		unset( $layout_options['half-grid'] );
 		return $layout_options;
+	}
+
+	/**
+	 * Replace the key for the layout switcher.
+	 *
+	 * @param  string $page_key
+	 * @return string
+	 */
+	public function lsx_layout_switcher_page_key( $page_key ) {
+		$lsx_search = LSX_Search::get_instance();
+		$page_key   = str_replace( '_search', '', $lsx_search->frontend->search_prefix );
+		return $page_key;
+	}
+
+	/**
+	 * CHange the default layout to a grid layout.
+	 *
+	 * @param  string $default
+	 * @return string
+	 */
+	public function lsx_layout_switcher_options_default( $default = 'grid' ) {
+		$lsx_search = LSX_Search::get_instance();
+		$default    = 'grid';
+		if ( isset( $lsx_search->frontend->options['display'][ $lsx_search->frontend->search_prefix . '_grid_list' ] ) && ! empty( $lsx_search->frontend->options['display'][ $lsx_search->frontend->search_prefix . '_grid_list' ] ) ) {
+			$default = $lsx_search->frontend->options['display'][ $lsx_search->frontend->search_prefix . '_grid_list' ];
+		}
+		return $default;
 	}
 }
